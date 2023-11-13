dead241dfdc0f9e6f6f75d9eec2e4dafd7b46120
Yosleivy Baez Acosta <yosleivy.baez@tropipay.com>
2023-11-13 02:19:05 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const app = express();
+
+app.use(express.json());
+app.post(
+  "/",
+  body("date").isDate({ format: "YYYY-MM-DD", strictMode: true }),
+  (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      return res.send(`Hello, ${req.query.person}!`);
+    }
+
+    res.send({ errors: result.array() });
+  }
+);
+
+app.listen(3000);
